25f1fcb37d04bdb7752cb481a806f3e0b96ec0e7
charly1020 <charly1020@gmail.com>
2017-11-16 12:24:02 -0300

    resize

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 //prod.js - production keys here!!
 
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SEjCRET,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  googleClientID: '351867171431-d9hgf5edutrpskdhu7inuaucr7fav4rk.apps.googleusercontent.com',
+  googleClientSecret: 'mqzHsLpfmtLv9Ni4Xj2k_yc8',
+  mongoURI: 'mongodb://charly:password@ds257485.mlab.com:57485/emaily-test',
+  cookieKey: 'aslkdfjlsadjflkasjdfoi'
 };
