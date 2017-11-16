513ddeb5844b9af77bbf18f4dcfaefef4071a5e8
charly1020 <charly1020@gmail.com>
2017-11-16 13:11:36 -0300

    just google

--- config/prod.js
+++ config/prod.js
@@ -3,6 +3,14 @@
 module.exports = {
   googleClientID: '351867171431-d9hgf5edutrpskdhu7inuaucr7fav4rk.apps.googleusercontent.com',
   googleClientSecret: 'mqzHsLpfmtLv9Ni4Xj2k_yc8',
-  mongoURI: 'mongodb://charly:password@ds257485.mlab.com:57485/emaily-test',
-  cookieKey: 'aslkdfjlsadjflkasjdfoi'
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
 };
+
+
+// module.exports = {
+//   googleClientID: '351867171431-d9hgf5edutrpskdhu7inuaucr7fav4rk.apps.googleusercontent.com',
+//   googleClientSecret: 'mqzHsLpfmtLv9Ni4Xj2k_yc8',
+//   mongoURI: 'mongodb://charly:password@ds257485.mlab.com:57485/emaily-test',
+//   cookieKey: 'aslkdfjlsadjflkasjdfoi'
+// };
